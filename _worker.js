b77372e994b39b2456bb0dd50f2e4a47b853804c
jjw3520 <jjw3520@naver.com>
2026-01-26 14:58:56 +0000

    chore: Remove Workers configuration for Pages deployment

--- _worker.js
+++
@@ -1,9 +0,0 @@
-import { serve } from 'wrangler:site';
-
-export default {
-  async fetch(request, env, ctx) {
-    // This function will automatically serve static assets from the [site] bucket
-    // and handle routing, including serving index.html for the root path.
-    return serve(request, env, ctx);
-  },
-};
