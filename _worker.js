266176839bed6ca613380a928b694019b8965b80
jjw3520 <jjw3520@naver.com>
2026-01-26 14:48:39 +0000

    refactor: Remove dummy worker and rely on --assets flag

--- _worker.js
+++
@@ -1,7 +0,0 @@
-// This is a dummy worker file to satisfy Wrangler.
-// The actual static site serving is handled by the [site] configuration in wrangler.toml.
-export default {
-  fetch() {
-    return new Response("This worker should not be called directly.", { status: 404 });
-  },
-};
